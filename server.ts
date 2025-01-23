f81b00b4e64c943b8480acfec9cb41f6740fdb04
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2025-01-22 23:23:42 -0800

    extraneous stuff

--- server.ts
+++ server.ts
@@ -1,11 +1,4 @@
 import { createRequestHandler } from "react-router";
-// import { type ServerBuild } from "react-router";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore This file won’t exist if it hasn’t yet been built
-import * as build from "./build/server"; // eslint-disable-line import/no-unresolved
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-// const handleRemixRequest = createRequestHandler(build as any as ServerBuild);
 
 const requestHandler = createRequestHandler(() => import("virtual:react-router/server-build"), import.meta.env.MODE);
 
